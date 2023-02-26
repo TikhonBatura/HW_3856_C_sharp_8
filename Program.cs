a08cb4cc8df813320953e21e03dc52c8b80acfff
Tikhon <loki_deleoni@live.com>
2023-02-26 18:03:06 +0300

    Done Task 58 & 60

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ Start();
 void Start()
 {
     while (true)
-    {      
+    {
         System.Console.WriteLine("\nЗадача 54: Задайте двумерный массив. \r\nНапишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
         System.Console.WriteLine("\nЗадача 56: Задайте прямоугольный двумерный массив. \r\nНапишите программу, которая будет находить строку с наименьшей суммой элементов.");
         System.Console.WriteLine("\nЗадача 58: Задайте две матрицы. \r\nНапишите программу, которая будет находить произведение двух матриц.");
@@ -17,7 +17,7 @@ void Start()
 
         switch (numTask)
         {
-            case 0: return; break;
+            case 0: return;
 
 
 
@@ -66,25 +66,31 @@ void Start()
             case 58:
                 Console.Clear();
 
-                int rowsOne = EnterNumber("Enter for 1th matrix row size: ");
-                int columsOne = EnterNumber("Enter for 1th matrix colums size: ");
-                int minValueOne = EnterNumber("\nEnter MinValue for 1th matrix ");
-                int maxValueOne = EnterNumber("Enter MaxValue for 1th matrix ");
-                int [,] matrixOne = GetMatrix(rowsOne, columsOne, minValueOne, maxValueOne);
+
+                int rowsOne = EnterNumber("Enter for A matrix row size: ");
+                int columsOne = EnterNumber("Enter for A matrix colums size: ");
+                int minValueOne = EnterNumber("\nEnter MinValue for A matrix ");
+                int maxValueOne = EnterNumber("Enter MaxValue for A matrix ");
+                int[,] matrixOne = GetMatrix(rowsOne, columsOne, minValueOne, maxValueOne);
                 System.Console.WriteLine();
-                
-                int rowsTwo = EnterNumber("Enter for 2th matrix row size: ");
-                int columsTwo = EnterNumber("Enter for 2th matrix colums size: ");
-                int minValueTwo = EnterNumber("\nEnter MinValue for 2th matrix ");
-                int maxValueTwo = EnterNumber("Enter MaxValue for 2th matrix ");
-                int [,] matrixTwo = GetMatrix(rowsTwo, columsTwo, minValueTwo, maxValueTwo);
+
+                int rowsTwo = EnterNumber("Enter for B matrix row size: ");
+                int columsTwo = EnterNumber("Enter for B matrix colums size: ");
+                int minValueTwo = EnterNumber("\nEnter MinValue for B matrix ");
+                int maxValueTwo = EnterNumber("Enter MaxValue for B matrix ");
+                int[,] matrixTwo = GetMatrix(rowsTwo, columsTwo, minValueTwo, maxValueTwo);
                 System.Console.WriteLine();
-                
+
+                System.Console.WriteLine("matrix A:");
                 PrintMatrix(matrixOne);
                 Console.WriteLine();
+                System.Console.WriteLine("matrix B:");
                 PrintMatrix(matrixTwo);
                 Console.WriteLine();
 
+
+                System.Console.WriteLine("matrix C = matix A x matrix B:");
+                PrintMatrix(matrixMultiply(matrixOne, matrixTwo));
 
                 break;
 
@@ -92,10 +98,22 @@ void Start()
             case 60:
                 Console.Clear();
 
+                int X = EnterNumber("Enter X for 3D matrix: ");
+                int Y = EnterNumber("Enter Y for 3D matrix: ");
+                int Z = EnterNumber("Enter Z for 3D matrix: ");
 
+
+                if (X*Y*Z > 90 || X <= 0 || Y <= 0 || Z <= 0)
+                System.Console.WriteLine("\nError.  X*Y*Z > 90 or <=0.");
+                else
+                {
+                int[,,] matrix3D = Get3DMatrix(X, Y, Z);
+                System.Console.WriteLine();
+                Print3DMatrix(matrix3D);
+                }
                 break;
 
-        default: System.Console.WriteLine("error"); break;
+            default: System.Console.WriteLine("error"); break;
 
         }
     }
@@ -204,21 +222,76 @@ void FindMinSumRows(int[,] matrix)
             rowSum += matrix[i, j];
         }
 
-        System.Console.WriteLine($"In row {i+1} sum = {rowSum}");
+        System.Console.WriteLine($"In row {i + 1} sum = {rowSum}");
 
         if (i == 0)
         {
             rowMinSum = rowSum;
-            rowMinSumIndex = i+1;
-        }    
+            rowMinSumIndex = i + 1;
+        }
         else if (rowSum < rowMinSum)
         {
             rowMinSum = rowSum;
-            rowMinSumIndex = i+1;
-        
+            rowMinSumIndex = i + 1;
+
         }
-            
+
     }
     System.Console.WriteLine($"\nMinimal sum in row {rowMinSumIndex}");
 }
-    
+
+int[,] matrixMultiply(int[,] A, int[,] B)
+{
+    if (A.GetLength(1) != B.GetLength(0))
+        System.Console.WriteLine("\nError!\r\nThe number of rows in 1th matrix must be equal to the number of columns 2th matrix\n");
+
+    int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < B.GetLength(1); j++)
+        {
+            for (int k = 0; k < B.GetLength(0); k++)
+            {
+                C[i, j] += A[i, k] * B[k, j];
+            }
+        }
+    }
+    return C;
+}
+
+int[,,] Get3DMatrix(int X, int Y, int Z)
+{
+    int[,,] matrix = new int[X, Y, Z];
+    int value = 10;
+    for (int i = 0; i < X; i++)
+    {
+        for (int j = 0; j < Y; j++)
+        {
+
+            for (int k = 0; k < Z; k++)
+            {
+
+                matrix[i, j, k] = value;
+                value += 1;
+            }
+            
+        }
+    }
+    return matrix;
+}
+
+void Print3DMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                System.Console.Write("{0,4}", $"{matrix[i, j, k]} ({i},{j},{k}) ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
